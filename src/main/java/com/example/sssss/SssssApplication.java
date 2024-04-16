f0ebca143839339c3773610607b055d3ac5e1c28
jjaladi5138 <jjaladi@uab.edu>
2024-04-16 12:32:03 -0400

    hhhh

--- src/main/java/com/example/sssss/SssssApplication.java
+++ src/main/java/com/example/sssss/SssssApplication.java
@@ -8,7 +8,7 @@ public class SssssApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("jaswanth jaladi hello how are dnnddddddyjjjjjjjjjjjjjjjjjjjjjjjjjjou");
+		System.out.println("jaswanth jaladi hello how are sssss");
 		SpringApplication.run(SssssApplication.class, args);
 	}
 
