a49ebcad304d9d1247408d3c0d44d3f14f9df8ff
jjaladi5138 <jjaladi@uab.edu>
2024-04-16 12:09:48 -0400

    Initial commit

---
+++ src/test/java/com/example/sssss/SssssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sssss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SssssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
